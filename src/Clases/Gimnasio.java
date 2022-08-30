0b17c24bd2de0ffde47457fb2f88baf4de655713
OreJoaquin <orellanoj066@hotmail.com>
2022-08-30 11:09:03 -0300

    agregue get y set a gimansio

--- src/Clases/Gimnasio.java
+++ src/Clases/Gimnasio.java
@@ -7,4 +7,31 @@ public class Gimnasio {
     private ArrayList<Entrenador> listaEntrenador = new ArrayList();
     private String codigoSeguridad = "12345";
 
+   
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(ArrayList<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public ArrayList<Entrenador> getListaEntrenador() {
+        return listaEntrenador;
+    }
+
+    public void setListaEntrenador(ArrayList<Entrenador> listaEntrenador) {
+        this.listaEntrenador = listaEntrenador;
+    }
+
+    public String getCodigoSeguridad() {
+        return codigoSeguridad;
+    }
+
+    public void setCodigoSeguridad(String codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+    
+    
+
 }
